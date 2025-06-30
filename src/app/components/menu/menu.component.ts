debc751408eed7d15e3411ae45de95f3ed6c98e5
AndyLC346 <149219743+AndyLC346@users.noreply.github.com>
2025-06-30 03:42:28 -0500

    MusicaMenu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -19,5 +19,22 @@ import { RouterLink } from '@angular/router';
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+ isMuted = true; // comienza silenciado
+ volumenPersonalizado = 0.1; // para poner la cantidad de volumen
 
+  toggleMute(audio: HTMLAudioElement) {
+    if (this.isMuted) {
+      // Desmutea y fija volumen personalizado
+      this.isMuted = false;
+      audio.muted = false;
+      audio.volume = this.volumenPersonalizado;
+      if (audio.paused) {
+        audio.play();
+      }
+    } else {
+      // Mutea
+      this.isMuted = true;
+      audio.muted = true;
+    }
+  }
 }
