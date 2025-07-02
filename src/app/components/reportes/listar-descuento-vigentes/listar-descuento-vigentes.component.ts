d00d4f2fc06c88f20e639e885008256afeae0db5
JorgeSanchez12-ctrl <U202117777@upc.edu.pe>
2025-07-02 16:16:20 -0500

    DescuentoVigentesPar2

--- src/app/components/reportes/listar-descuento-vigentes/listar-descuento-vigentes.component.ts
+++ src/app/components/reportes/listar-descuento-vigentes/listar-descuento-vigentes.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChartDataset, ChartOptions, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+import { CarritocompraService } from '../../../services/carritocompra.service';
+import { DescuentoVigentesDTO } from '../../../models/descuentovigenteDTO';
+import { DescuentoService } from '../../../services/descuento.service';
 
 @Component({
   selector: 'app-listar-descuento-vigentes',
-  imports: [],
+  standalone: true,
+  imports: [BaseChartDirective],
   templateUrl: './listar-descuento-vigentes.component.html',
-  styleUrl: './listar-descuento-vigentes.component.css'
+  styleUrl: './listar-descuento-vigentes.component.css',
 })
-export class ListarDescuentoVigentesComponent {
+export class ListarDescuentoVigentesComponent implements OnInit {
+  barChartOptions: ChartOptions = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+      },
+    },
+  };
 
+  barChartLabels: string[] = [];
+  barChartType: ChartType = 'doughnut';
+  barChartLegend = true;
+  barChartData: ChartDataset[] = [];
+
+  constructor(private dC: DescuentoService) {}
+
+  ngOnInit(): void {
+    this.dC.DescVigente().subscribe((data: DescuentoVigentesDTO[]) => {
+      this.barChartLabels = data.map(item => item.codigoDescuento);
+      this.barChartData = [
+        {
+          data: data.map(item => item.porcentajeDescuento),
+          label: 'Descuentos Vigentes (%)',
+          backgroundColor: [
+            '#f44336', '#2196f3', '#4caf50', '#ff9800', '#9c27b0', '#00bcd4',
+          ],
+          borderColor: '#ffffff',
+          borderWidth: 1,
+        },
+      ];
+    });
+  }
 }
