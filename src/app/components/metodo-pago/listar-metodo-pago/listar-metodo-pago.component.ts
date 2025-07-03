c84555f1c7b39b032a25bb40f6f1e1e71e475f7c
U20211E436 <U20211E436@upc.edu.pe>
2025-07-03 10:07:52 -0500

    paginador

--- src/app/components/metodo-pago/listar-metodo-pago/listar-metodo-pago.component.ts
+++ src/app/components/metodo-pago/listar-metodo-pago/listar-metodo-pago.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,11 +6,19 @@ import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { MetodoPago } from '../../../models/metodo-pago';
 import { MetodoPagoService } from '../../../services/metodo-pago.service';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-listar-metodo-pago',
   standalone: true,
-  imports: [MatTableModule, CommonModule, MatButtonModule, MatIconModule, RouterLink],
+  imports: [
+    MatTableModule,
+     CommonModule,
+      MatButtonModule, 
+      MatIconModule, 
+      RouterLink,
+      MatPaginatorModule,
+    ],
   templateUrl: './listar-metodo-pago.component.html',
   styleUrl: './listar-metodo-pago.component.css',
 })
@@ -18,6 +26,8 @@ export class ListarMetodoPagoComponent implements OnInit {
   dataSource: MatTableDataSource<MetodoPago> = new MatTableDataSource();
   displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4', 'c5'];
 
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   constructor(private mpS: MetodoPagoService) {}
 
   ngOnInit(): void {
@@ -28,6 +38,9 @@ export class ListarMetodoPagoComponent implements OnInit {
       this.dataSource = new MatTableDataSource(data);
     });
   }
+   ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+   }
 
   eliminar(id: number) {
     this.mpS.delete(id).subscribe(() => {
--- src/app/components/notificaciones/listar-notificaciones/listar-notificaciones.component.ts
+++ src/app/components/notificaciones/listar-notificaciones/listar-notificaciones.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,17 +6,26 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Notificaciones } from '../../../models/notificaciones';
 import { NotificacionesService } from '../../../services/notificaciones.service';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-listar-notificaciones',
   standalone: true,
-  imports: [MatTableModule, CommonModule, MatButtonModule, MatIconModule, RouterModule],
+  imports: [
+    MatTableModule, 
+    CommonModule, 
+    MatButtonModule,
+    MatPaginatorModule, 
+    MatIconModule,
+     RouterModule],
   templateUrl: './listar-notificaciones.component.html',
   styleUrl: './listar-notificaciones.component.css',
 })
 export class ListarNotificacionesComponent implements OnInit {
   dataSource: MatTableDataSource<Notificaciones> = new MatTableDataSource();
   displayedColumns: string[] = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6'];
+
+   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private nS: NotificacionesService) {}
 
@@ -30,6 +39,9 @@ export class ListarNotificacionesComponent implements OnInit {
       this.dataSource = new MatTableDataSource(data);
     });
   }
+    ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+}
 
   eliminar(id: number) {
     this.nS.delete(id).subscribe(() => {
